225d917cd91b8fbbb7254b82b4e4a173d292637c
Jaga <jagadheesh6@gmail.com>
2022-12-04 18:36:04 +0530

    chore - aws creds

--- src/database/dynamodb.ts
+++ src/database/dynamodb.ts
@@ -1,4 +1,10 @@
 import AWS from 'aws-sdk';
 import 'dotenv/config';
 
-
+const awsConfig = {
+    apiVersion: "2010-12-01",
+    accessKeyId: process.env.AWS_ACCESS_KEY,
+    accessSecretKey: process.env.AWS_SECRET_KEY,
+    region: process.env.AWS_REGION
+}
+AWS.config.update(awsConfig);
